e87b924fec67962ebf0a79e4dc2b0e9725a1824c
Leefranklyn <benimarolee@gmail.com>
2024-01-25 09:44:43 +0100

    feature: added the form components

---
+++ src/components/forms/ContractorSignUpForm.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const ContractorSignUpForm = () => {
+  return (
+    <div>ContractorSignUpForm</div>
+  )
+}
---
+++ src/components/forms/SupplierSignUpForm.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const SupplierSignUpForm = () => {
+  return (
+    <div>SupplierSignUpForm</div>
+  )
+}
