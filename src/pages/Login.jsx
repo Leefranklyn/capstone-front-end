8d58259eb40601cfe347d7dc9b96db0a990de1ef
Leefranklyn <benimarolee@gmail.com>
2024-01-27 01:55:40 +0100

    feature: worked on the login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,45 @@
-import React from 'react'
+/* eslint-disable react/no-unescaped-entities */
+import React from 'react';
+import { 
+  Text, Box, Flex, FormControl, FormLabel, Input, Button, Image, Link as ChakraLink 
+} from '@chakra-ui/react';
 import { motion } from 'framer-motion';
-
+import { Link as ReactRouterLink } from 'react-router-dom';
 
 export const Login = () => {
   return (
-    <>
-    <motion.div initial={{ opacity: 0 }}
-    animate={{ opacity: 1 }}
-    exit={{ opacity: 0 }} style={{ padding: "40px"}}>Login</motion.div>
-    </>
-  )
-}
+    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+      <Flex direction="column" alignItems="center" paddingX={{ base: 4, md: 10 }} gap={{ base: 6, md: 10 }}>
+        <Text fontSize={{ base: '24px', md: '38px' }} lineHeight={{ base: '32px', md: '44px' }} fontWeight={600} fontFamily="roboto-serif" marginTop="20px">Login now</Text>
+        <Flex direction={{ base: 'column', md: 'row' }} gap={{ base: 6, md: '5%' }}>
+          <Box flex="1">
+            <Image src='https://res.cloudinary.com/dwinqwsit/image/upload/v1706305160/image_1_ndzbsd.png' alt='login image' width="100%" height="auto" marginBottom={{ base: 6, md: 0 }} />
+          </Box>
+          <Box flex="1">
+            <Flex direction="column" gap={6} alignItems="center">
+              <FormControl>
+                <FormLabel fontSize="24px" lineHeight="32px" fontWeight={400} fontFamily="roboto-serif">Email</FormLabel>
+                <Input placeholder='Input your email address' width="100%" height="64px" paddingLeft="30px" fontSize="18px" fontFamily="roboto-serif" border="1px solid black" opacity={0.5}/>
+              </FormControl>
+              <FormControl>
+                <FormLabel fontSize="24px" lineHeight="32px" fontWeight={400} fontFamily="roboto-serif">Password</FormLabel>
+                <Input placeholder='Password' height="64px" paddingLeft="30px" fontSize="18px" fontFamily="roboto-serif" border="1px solid black" opacity={0.5}/>
+              </FormControl>
+              <Button as={ReactRouterLink} to="/login" width="100%" height="64px" marginTop={10} bg="customGreen.500" border="1px solid #09544d" _hover={{ bg: "customGreen.500" }}>
+                <Text color="white" fontWeight={400} fontFamily="roboto-serif">Log In</Text>
+              </Button>
+              <Text fontWeight={400} fontSize="18px" fontFamily="roboto-serif">OR</Text>
+              <Button as={ReactRouterLink} to="/signup" width="100%" height="64px" bg="white" border="1px solid #09544d" _hover={{ bg: "white" }}>
+                <Text color="customGreen.400" fontWeight={400} fontFamily="roboto-serif">Sign Up</Text>
+              </Button>
+              <Text fontSize="18px" fontFamily="roboto-serif" fontWeight={400}>
+                If you can't remember your login details, <ChakraLink as={ReactRouterLink} to="/resetpassword" color="#09544D" fontSize="18px" fontFamily="roboto-serif" fontWeight={400}>Reset password</ChakraLink>
+              </Text>
+            </Flex>
+          </Box>
+        </Flex>
+      </Flex>
+    </motion.div>
+  );
+};
+
